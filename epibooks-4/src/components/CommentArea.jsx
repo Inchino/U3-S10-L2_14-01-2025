44b150761290947526b48b0048a356ba0af7d2dd
Inchino <alessandro.incalza22@gmail.com>
2025-01-14 16:16:24 +0100

    CommentArea

--- epibooks-4/src/components/CommentArea.jsx
+++ epibooks-4/src/components/CommentArea.jsx
@@ -1,10 +1,61 @@
-import { Component } from 'react'
 import CommentList from './CommentList'
 import AddComment from './AddComment'
 import Loading from './Loading'
 import Error from './Error'
+import { useState, useEffect } from 'react'
 
-class CommentArea extends Component {
+const CommentArea = (props) => {
+  const [comments, setComments] = useState ([]);
+  const [isLoading, setIsLoading] = useState (false);
+  const [isError, setIsError] = useState (false);
+
+  useEffect(() => {
+  const componentDidUpdate = async () => {
+    if (props.asin) {
+      setIsLoading(true);
+      try {
+        let response = await fetch(
+          'https://striveschool-api.herokuapp.com/api/comments/' +
+            props.asin,
+          {
+            headers: {
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDFlZGQyMjA3MTAwMTVkZTJmNWQiLCJpYXQiOjE3MzY3Nzk0ODAsImV4cCI6MTczNzk4OTA4MH0.fJ7dtMaMlLeyzfmyTf7EEVYYim0_cNiEIGY_W5HIvM4',
+            },
+          }
+        )
+        console.log(response)
+        if (response.ok) {
+          let comments = await response.json()
+          setComments (comments);
+          setIsLoading (false);
+          setIsError (false);
+        } else {
+          setIsLoading (false);
+          setIsError (true);
+        }
+      } catch (error) {
+        console.log(error)
+        setIsLoading (false);
+        setIsError (true);
+      }
+    }
+  }
+  componentDidUpdate();
+}, [props.asin])
+
+    return (
+      <div className="text-center">
+        {isLoading && <Loading />}
+        {isError && <Error />}
+        <AddComment asin={props.asin} />
+        <CommentList commentsToShow={comments} />
+      </div>
+    )
+}
+
+export default CommentArea;
+
+{/*class CommentArea extends Component {
   state = {
     comments: [],
     isLoading: false,
@@ -82,4 +133,4 @@ class CommentArea extends Component {
   }
 }
 
-export default CommentArea
+export default CommentArea*/}
