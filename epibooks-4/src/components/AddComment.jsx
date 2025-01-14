9e6e66aa3ca27857cb6e06291952567f72bd5140
Inchino <alessandro.incalza22@gmail.com>
2025-01-14 15:38:31 +0100

    AddComment

--- epibooks-4/src/components/AddComment.jsx
+++ epibooks-4/src/components/AddComment.jsx
@@ -1,7 +1,85 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import { Button, Form } from 'react-bootstrap'
 
-class AddComment extends Component {
+const AddComment = (props) => {
+  
+  const [commentStr, setCommentStr] = useState('');
+  const [rate, setRate] = useState(1);
+  const [elementId, setElementId] = useState(props.asin);
+
+  useEffect(() => {
+    setElementId(props.asin);
+  }, [props.asin]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    try {
+      const commentObj = {
+        comment: commentStr,
+        rate: rate,
+        elementId: elementId,
+      };
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments',
+        {
+          method: 'POST',
+          body: JSON.stringify(commentObj),
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: 'Bearer <YOUR_TOKEN>',
+          },
+        }
+      );
+      if (response.ok) {
+        alert('Recensione inviata!');
+        setCommentStr('');
+        setRate(1);
+      } else {
+        throw new Error('Qualcosa è andato storto');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div className="my-3">
+      <Form onSubmit={sendComment}>
+        <Form.Group className="mb-2">
+          <Form.Label>Recensione</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci qui il testo"
+            value={commentStr}
+            onChange={(e) => setCommentStr(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Valutazione</Form.Label>
+          <Form.Control
+            as="select"
+            value={rate}
+            onChange={(e) => setRate(e.target.value)}
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Invia
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddComment;
+
+
+{/*class AddComment extends Component {
   state = {
     comment: {
       comment: '',
@@ -102,4 +180,4 @@ class AddComment extends Component {
   }
 }
 
-export default AddComment
+export default AddComment*/}
